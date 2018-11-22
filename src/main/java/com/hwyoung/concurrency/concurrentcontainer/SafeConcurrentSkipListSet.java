4eb37fa5f8e20aa41f3ce7edf0cb7902d5f87e66
Zihaoo <hwyoung.96@gmail.com>
2018-11-22 22:24:55 +0800

    feat(concurrentcontainer): 绝对线程安全容器（并发容器）demo测试

---
+++ src/main/java/com/hwyoung/concurrency/concurrentcontainer/SafeConcurrentSkipListSet.java
@@ -0,0 +1,57 @@
+package com.hwyoung.concurrency.concurrentcontainer;
+
+import com.hwyoung.concurrency.annotation.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * 线程安全并发容器类：TreeSet -> ConcurrentSkipListSet
+ *
+ */
+@Slf4j
+@ThreadSafe
+public class SafeConcurrentSkipListSet {
+	/**
+	 * 请求总数
+	 */
+	private static int clientTotal = 5000;
+
+	/**
+	 * 线程并发数
+	 */
+	private static int threadTotal = 200;
+
+	/**
+	 * set
+	 */
+	private static Set<Integer> set = new ConcurrentSkipListSet<>();
+
+	public static void main(String[] args) throws Exception{
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		for (int i = 0; i < clientTotal; i++){
+			final int count = i;
+			executorService.execute(() -> {
+				try {
+					semaphore.acquire();
+					update(count);
+					semaphore.release();
+				} catch (InterruptedException e) {
+					log.error("exception:",e);
+				}
+				countDownLatch.countDown();
+			});
+		}
+		countDownLatch.await();
+		executorService.shutdown();
+		log.info("size:{}",set.size());
+	}
+
+	private static void update(int i){
+		set.add(i);
+	}
+
+}
